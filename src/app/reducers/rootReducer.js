30477ff0d75f1e8661cb4e891d4b0dde32b70ef7
vbhavsar <vikramb@connecttel.com>
2020-05-13 22:24:38 -0500

    End of section 13

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -5,6 +5,7 @@ import eventReducer from './eventReducer';
 import modalReducer from '../../features/modals/modalReducer';
 import authReducer from '../../features/auth/authReducer';
 import asyncReducer from '../../features/async/asyncReducer';
+import { reducer as ReduxToastr } from 'react-redux-toastr';
 
 const rootReducer = combineReducers({
   form: FormReducer,
@@ -13,6 +14,7 @@ const rootReducer = combineReducers({
   modals: modalReducer,
   auth: authReducer,
   async: asyncReducer,
+  toastr: ReduxToastr,
 });
 
 export default rootReducer;
